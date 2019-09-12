9244fe1c51f62c4217996da1a341de85cf9087c9
Kim Sandberg <s@ndberg.dk>
2019-09-12 15:31:18 +0200

    Initial Commit

---
+++ test1/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  test1
+//
+//  Created by Kim Sandberg on 12/09/2019.
+//  Copyright © 2019 Kim Sandberg. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
